84c43661f934c2964d9094f7f1b14a912f505ba4
Nightrider0098 <hitesh08011999@gmail.com>
2020-12-28 15:44:19 +0530

    added media and server file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,73 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 8080;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors')
+const path = require('path')
+app.use(cors())
+const { data } = require('./data')
+const fs = require('fs')
 
-const setupServer = () => {
-  const app = express();
-  app.listen(3000);
-  app.use(cors());
-  app.set('port', 8080 || 3000);
-  return app;
-}
 
-setupServer()
+app.get('/api/image', (req, res) => {
+    let fileName = req.query.fileName;
+    let secret = req.query.secret;
+    let _path = path.join(__dirname, "public", "image", fileName);
+
+    if (secret === "secret") {
+        if (fs.existsSync(_path)) {
+
+            res.sendFile(_path)
+        }
+        else {
+            res.status(404)
+        }
+    }
+})
+
+app.get('/api/text', (req, res) => {
+    let fileName = req.query.fileName;
+    let secret = req.query.secret;
+    // console.log(fileName, secret,_path)
+    let _path = path.join(__dirname, "public", "text", fileName);
+    console.log(fileName, secret, _path)
+    if (secret === "secret") {
+        if (fs.existsSync(_path)) {
+
+            res.sendFile(_path)
+        }
+        else {
+            res.status(404).send()
+        }
+    }
+})
+
+
+app.get('/api/video', (req, res) => {
+    let fileName = req.query.fileName;
+    let secret = req.query.secret;
+    // console.log(fileName, secret,_path)
+    let _path = path.join(__dirname, "public", "video", fileName);
+    console.log(fileName, secret, _path)
+    if (secret === "secret") {
+        if (fs.existsSync(_path)) {
+
+            res.sendFile(_path)
+        }
+        else {
+            res.status(404).send()
+        }
+    }
+})
+
+app.get('/api/available', (req, res) => {
+    console.log("ex")
+    res.status(200).json(data)
+})
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+module.exports = app;
